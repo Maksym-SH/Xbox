0e4e26c6c782ca1b46466afb8792a750983966f1
Maksym-SH <maxim2002sherba4yk@gmail.com>
2021-11-12 20:42:01 +0300

    add small info in component

--- src/views/CheckedConsole.vue
+++ src/views/CheckedConsole.vue
@@ -28,7 +28,11 @@
         The redesigned control panel has been designed to speed up your access
         to games and entertainment as quickly as possible.
       </p>
-      <img src="@/assets/image/console-game.png" alt="" class="console__game" />
+      <img
+        src="@/assets/image/console-game.png"
+        alt="Console games"
+        class="console__game"
+      />
     </div>
     <div class="console__game-pass">
       <h3>FINE PERFORMANCE ADJUSTMENT</h3>
@@ -37,7 +41,28 @@
         sophisticated comfort, and instant transfer into the hands of the person
         you know.
       </p>
-      <img src="@/assets/image/controller.png" alt="" class="console__game" />
+      <img
+        src="@/assets/image/controller.png"
+        alt="Controller"
+        class="console__game"
+      />
+    </div>
+    <div class="container">
+      <div class="console__controller-desc">
+        <img
+          src="@/assets/image/xbox-controller.png"
+          alt="Xbox wirelles controller"
+        />
+        <p>
+          Experience the redesigned <span>Xbox</span> Wireless Controller and
+          enjoy its curved surfaces and sophisticated geometry for enhanced
+          gaming comfort. Never miss your target again with textured handles and
+          hybrid D-pad. Capture and share content easily with a dedicated Share
+          button. Quickly pair, play and switch between devices including<span>
+            Xbox Series X | S, Xbox One, Windows 10 PC, Android, and iOS.</span
+          >
+        </p>
+      </div>
     </div>
   </section>
 </template>
@@ -127,7 +152,8 @@ export default {
   width: 100%;
   margin-top: 64px;
   padding-top: 60px;
-  background: url(../assets/image/console-page.jpg) no-repeat center center;
+  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url(../assets/image/console-page.jpg) center center no-repeat;
   background-size: cover;
   background-attachment: fixed;
   &__image {
@@ -176,6 +202,7 @@ export default {
   &__game {
     margin-top: 30px !important;
     width: 50%;
+    max-width: 700px;
     @media (max-width: $media-sm) {
       width: 90%;
     }
@@ -237,6 +264,41 @@ export default {
     margin-top: 200px !important;
     span {
       color: $light-green;
+    }
+  }
+  &__controller-desc {
+    display: flex;
+    justify-content: space-between;
+    padding-bottom: 100px;
+    padding-top: 60px;
+    @media (max-width: $media-md) {
+      flex-direction: column;
+    }
+    img {
+      width: 40%;
+      height: 100%;
+      border-radius: 10px;
+      max-width: 800px;
+      @media (max-width: $media-md) {
+        width: 80%;
+        min-height: 200px;
+        margin: 0 auto;
+      }
+    }
+    p {
+      font-size: 20px;
+      margin-left: 50px !important;
+      text-align: center;
+      display: block;
+      font-weight: bold;
+      margin: 0 auto;
+      color: $white;
+      span {
+        color: $light-green;
+      }
+      @media (max-width: $media-md) {
+        margin: 30px auto 0px auto !important;
+      }
     }
   }
 }
